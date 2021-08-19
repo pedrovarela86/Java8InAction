4ae14f45803940a7208bafda249752c2d92ea57d
Pedro Varela <pedrovarela86@gmail.com>
2021-08-18 22:29:19 -0400

    Add static apple list

---
+++ src/AppleList.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.List;
+
+public abstract class AppleList {
+    public static List<Apple> appleList = Arrays.asList(
+            new Apple(100, "green"),
+            new Apple(110, "green"),
+            new Apple(150, "yellow"),
+            new Apple(150, "yellow"),
+            new Apple(250, "red"),
+            new Apple(50, "red"),
+            new Apple(40, "red"),
+            new Apple(160, "red"),
+            new Apple(162, "red"),
+            new Apple(210, "green"),
+            new Apple(110, "red"),
+            new Apple(100, "green"),
+            new Apple(210, "red"),
+            new Apple(210, "yellow"));
+}
